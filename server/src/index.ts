09aef2c1e5d1dd1f469e770bd8e7435b1f9f96bd
KmK33 <kmk112003@gmail.com>
2024-09-01 16:33:18 +0530

    initial

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+//routes imports
+import dashboardRoutes from './routes/dashboardRoutes';
+
+//config
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+
+//ROUTES
+app.use("/dashboard", dashboardRoutes)
+//SERVER
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log("Server running on", port) })
