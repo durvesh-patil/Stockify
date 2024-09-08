f01fc327395ae01b84af22ca2933406ca5f00710
KmK33 <kmk112003@gmail.com>
2024-09-08 21:50:24 +0530

    pm config

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,5 +31,5 @@ app.use("/expenses", expenseRoutes) //http://localhost:5000/expenses
 
 
 //SERVER
-const port = process.env.PORT || 5000;
-app.listen(port, () => { console.log("Server running on", port) })
+const port = Number(process.env.PORT) || 5000;
+app.listen(port, "0.0.0.0", () => { console.log("Server running on", port) })
